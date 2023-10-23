da57a3f1396143dc26f3ffece7d0dee221867147
callumahern <callum.ahern@simplybusiness.co.uk>
2023-10-23 16:04:33 +0100

    added additional views for other models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'home#homepage'
+
   get 'mood/new', to: 'mood#new'
   post 'mood/new', to: 'mood#create'
+
+  get 'food/new', to: 'food#new'
+  post 'food/new', to: 'food#create'
+
+  get 'habits/new', to: 'habits#new'
+  post 'habits/new', to: 'habits#create'
 end
