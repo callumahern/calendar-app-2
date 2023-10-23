f2152a6f8f89573e149090437bc62c095aeef45a
callumahern <callum.ahern@simplybusiness.co.uk>
2023-10-23 17:48:03 +0100

    habit list (incomplete)

--- app/models/habits.rb
+++ app/models/habits.rb
@@ -1,3 +1,11 @@
-class Habits < ApplicationRecord
+# frozen_string_literal: true
 
+class Habits < ApplicationRecord
+  HABIT_LIST = [
+    'Read',
+    'Yoga',
+    'Run',
+    'Go to the gym',
+    'Clean house'
+  ].freeze
 end
