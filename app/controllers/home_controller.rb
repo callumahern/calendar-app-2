3f624a7f5251da7eebbe52ac97e4f101d7ed302d
callumahern <callum.ahern@simplybusiness.co.uk>
2023-10-23 15:03:30 +0100

    entry models added to controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def homepage; end
+  ENTRY_MODELS = [Food, Finance, Mood, Habits].freeze
+  def homepage
+    s
+    @entries = @entries = ENTRY_MODELS.flat_map do |model|
+      model.where(entry_date: DateTime.now.beginning_of_month.beginning_of_week..DateTime.now.end_of_month.end_of_week)
+    end
+  end
 end
