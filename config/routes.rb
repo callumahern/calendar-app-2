e6eb92538ce193e6b123b9d4eea645a77ef53a9f
callumahern <callum.ahern@simplybusiness.co.uk>
2023-10-23 15:42:17 +0100

    front end capability to add mood

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'home#homepage'
+  get 'new', to: 'mood#new'
+  post 'new', to: 'mood#create'
 end
