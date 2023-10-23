1c0189a7fa99b308977f309f43c38719398ae2b5
callumahern <callum.ahern@simplybusiness.co.uk>
2023-10-23 14:06:18 +0100

    model / tables created

---
+++ db/migrate/20231023125505_create_food.rb
@@ -0,0 +1,11 @@
+class CreateFood < ActiveRecord::Migration[7.0]
+  def change
+    create_table :foods do |t|
+      t.string :title
+      t.boolean :healthy
+      t.datetime :entry_date
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20231023125524_create_mood.rb
@@ -0,0 +1,10 @@
+class CreateMood < ActiveRecord::Migration[7.0]
+  def change
+    create_table :moods do |t|
+      t.string :title
+      t.datetime :entry_date
+
+      t.timestamps
+    end
+  end
+end
