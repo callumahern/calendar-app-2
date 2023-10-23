c9c31c8c510a2ca8caf597e3b0087c652ea7f717
callumahern <callum.ahern@simplybusiness.co.uk>
2023-10-23 15:57:11 +0100

    mood can only ever be one of 3 options

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'home#homepage'
-  get 'new', to: 'mood#new'
-  post 'new', to: 'mood#create'
+  get 'mood/new', to: 'mood#new'
+  post 'mood/new', to: 'mood#create'
 end
